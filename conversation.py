673b6f349e91560c43aadd21081b31071394379e
Sudarshan Panchal <panchalsudarshan@sggs.ac.in>
2017-04-13 06:28:11 +0000

    diff_file2

--- conversation.py
+++ conversation.py
@@ -1,13 +1,13 @@
 from watson_developer_cloud import ConversationV1
 
 def conversation():
-
+	
 	conversation = ConversationV1(
-    username='81cae901-ee0e-4066-b333-c6d9cc5532ec',
-    password='NCdy2rD8GQ5N',
-    version='2017-02-03')
+	username='81cae901-ee0e-4066-b333-c6d9cc5532ec',
+	password='NCdy2rD8GQ5N',
+	version='2017-02-03')
+	
+	conv_workspace_id = 'e5fa2b42-e839-4e1b-9c6d-4d3ca9a93330'
+	context={}
     
-    conv_workspace_id = 'e5fa2b42-e839-4e1b-9c6d-4d3ca9a93330'
-    context={}
-    
-    return conversation,conv_workspace_id,context
+	return conversation,conv_workspace_id,context
--- text_to_speech.py
+++ text_to_speech.py
@@ -3,8 +3,8 @@ from watson_developer_cloud import TextToSpeechV1
 def text_to_speech():
 
 	text_to_speech = TextToSpeechV1(
-    username='8921eb13-5eb4-4286-9add-5a7870ba4ecf',
-    password='tKJBMhuw42nL',
-    x_watson_learning_opt_out=True)
+	username='8921eb13-5eb4-4286-9add-5a7870ba4ecf',
+	password='tKJBMhuw42nL',
+	x_watson_learning_opt_out=True)
     
-    return text_to_speech
+	return text_to_speech
