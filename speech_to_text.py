970af207844e0ce139cc6c500df11212fbffb7f7
Sudarshan Panchal <panchalsudarshan@sggs.ac.in>
2017-04-18 15:17:27 +0000

    function error

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,7 +1,7 @@
 import json
 from watson_developer_cloud import SpeechToTextV1
 
-def speech_to_text_fun:
+def speech_to_text_fun():
 	speech_to_text = SpeechToTextV1(
 		username='c4a1eb85-0246-4488-8f9d-fd6ce44a9ff6',
 		password='TzQgfJ53bwXtD',
