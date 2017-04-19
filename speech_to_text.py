4c59058121651be09910f2d4b5b9c311572d6a44
Sudarshan Panchal <panchalsudarshan@sggs.ac.in>
2017-04-19 05:26:53 +0000

    s2t password error

--- speech_to_text.py
+++ speech_to_text.py
@@ -4,6 +4,6 @@ from watson_developer_cloud import SpeechToTextV1
 def speech_to_text_fun():
 	speech_to_text = SpeechToTextV1(
 		username='c4a1eb85-0246-4488-8f9d-fd6ce44a9ff6',
-		password='TzQgfJ53bwXtD',
+		password='TzQgfJ53bwXt',
 		x_watson_learning_opt_out=False)
 	return speech_to_text
